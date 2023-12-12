f3593930c1132cd5e9bad68e3cc1074576bafe30
villegust <ville.gust@gmail.com>
2023-12-12 10:12:49 +0100

    Potential fix of redirect errors

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <Routes>
       <Route index path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
+      <Route path="about" element={<About />} />
     </Routes>
   );
 }
